b5c75f2d6fc57e20d55ffa55a1742e0f8c77766c
Jeff Black <jeff@jeffblack360.com>
2021-02-27 21:22:41 -0600

    DEV small change

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,5 +1,5 @@
 package com.example.servingwebcontent;
-
+// outside workspace
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
